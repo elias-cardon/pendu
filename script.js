c7d488674121fbdcfe7d4ed049807f0d79c0f089
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2021-04-21 17:40:15 +0200

    Début du script -> Début liste de mots

---
+++ script.js
@@ -0,0 +1,42 @@
+const wordElement = document.getElementById("word");
+const wronglettersElement = document.getElementById("wrong-letter");
+const playAgainButton = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+const finalMessageRevealWord = document.getElementById("final-message-reveal-word");
+const figureParts = document.getElementById(".figure-parts");
+
+const words = [
+    "ornithorynque",
+    "Jobba",
+    "vacances",
+    "fatigue",
+    "fetes",
+    "joyeux",
+    "paranormal",
+    "raisin",
+    "vigne",
+    "voiture",
+    "camion",
+    "moto",
+    "roue",
+    "verre",
+    "bocal",
+    "biberon",
+    "cartouche",
+    "lit",
+    "indien",
+    "matelas",
+    "parapluie",
+    "parasol",
+    "paratonnerre",
+    "tondeuse",
+    "parachute",
+    "pelouse",
+    "herbe",
+    "avion",
+    "cacahuete",
+    "creepypasta",
+    "champignon"
+]
